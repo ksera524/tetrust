15282841b725d4db17a286c0b3e3d6b3b14aaec8
ksera524 <ksera631@gmail.com>
2023-02-27 16:10:12 +0900

    chap6

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,35 @@
 use getch_rs::{Getch, Key};
+mod block;
+mod game;
+use game::*;
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
-#[derive(Clone, Copy)]
-enum BlockKind {
-    I,
-    O,
-    S,
-    Z,
-    J,
-    L,
-    T,
-}
-
-const FIELD_WIDTH: usize = 11 + 2;
-const FIELD_HEIGHT: usize = 20 + 1;
-type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
-
-type BlockShape = [[usize; 4]; 4];
-const BLOCKS: [BlockShape; 7] = [
-    // Iブロック
-    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
-    // Oブロック
-    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
-    // Sブロック
-    [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]],
-    // Zブロック
-    [[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
-    // Jブロック
-    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
-    // Lブロック
-    [[0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
-    // Tブロック
-    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
-];
-
-struct Position {
-    x: usize,
-    y: usize,
-}
-
-fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
-    for y in 0..4 {
-        for x in 0..4 {
-            if field[y + pos.y][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
-                return true;
-            }
-        }
-    }
-    false
-}
-
 fn main() {
-    let field = [
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
-
-    let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
+    let game = Arc::new(Mutex::new(Game::new()));
     // 画面クリア
     println!("\x1b[2J\x1b[H\x1b[?25l");
-    draw(&field, &pos.lock().unwrap());
+    draw(&game.lock().unwrap());
 
     {
-        let pos = Arc::clone(&pos);
+        let game = Arc::clone(&game);
         let _ = thread::spawn(move || loop {
             thread::sleep(time::Duration::from_millis(1000));
-            let mut pos = pos.lock().unwrap();
+            let mut game = game.lock().unwrap();
             let new_pos = Position {
-                x: pos.x,
-                y: pos.y + 1,
+                x: game.pos.x,
+                y: game.pos.y.checked_add(1).unwrap_or_else(|| game.pos.y),
             };
-            if !is_collision(&field, &new_pos, BlockKind::I) {
-                *pos = new_pos;
+
+            if !is_collision(&game.field, &new_pos, game.block) {
+                game.pos = new_pos;
+            } else {
+                fix_block(&mut game);
+                erase_line(&mut game.field);
+                game.pos = Position::init();
+                game.block = rand::random();
             }
-            draw(&field, &pos);
+            draw(&game);
         });
     }
 
@@ -106,59 +42,33 @@ fn main() {
                 return;
             }
             Ok(Key::Left) => {
-                let mut pos = pos.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x - 1,
-                    y: pos.y,
+                    x: game.pos.x.checked_sub(1).unwrap_or(game.pos.x),
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
-                    *pos = new_pos;
-                }
+                move_block(&mut game, new_pos);
+                draw(&game);
             }
             Ok(Key::Right) => {
-                let mut pos = pos.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x + 1,
-                    y: pos.y,
+                    x: game.pos.x.checked_add(1).unwrap_or_else(|| game.pos.x),
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
-                    *pos = new_pos;
-                }
+                move_block(&mut game, new_pos);
+                draw(&game);
             }
             Ok(Key::Down) => {
-                let mut pos = pos.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x,
-                    y: pos.y + 1,
+                    x: game.pos.x,
+                    y: game.pos.y.checked_add(1).unwrap_or_else(|| game.pos.y),
                 };
-                if !is_collision(&field, &new_pos, BlockKind::I) {
-                    *pos = new_pos;
-                }
+                move_block(&mut game, new_pos);
+                draw(&game);
             }
             _ => (),
         }
-    }
-}
-
-fn draw(field: &Field, pos: &Position) {
-    let mut field_buf = field.clone();
-    for y in 0..4 {
-        for x in 0..4 {
-            if BLOCKS[BlockKind::I as usize][y][x] == 1 {
-                field_buf[y + pos.y][x + pos.x] = 1;
-            }
-        }
-    }
-
-    println!("\x1b[H"); // カーソルを先頭に移動
-    for y in 0..FIELD_HEIGHT {
-        for x in 0..FIELD_WIDTH {
-            if field_buf[y][x] == 0 {
-                print!("・");
-            } else {
-                print!("[]");
-            }
-        }
-        println!();
     }
 }
